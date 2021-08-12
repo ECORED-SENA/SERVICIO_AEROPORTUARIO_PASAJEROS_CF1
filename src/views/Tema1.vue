6bad3e47d2cdcd9c5680d6b6411a29b742baf81b
luisEfe80 <luismendez80@hotmail.com>
2021-08-11 23:18:01 -0400

    Ajuste de contenido

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -56,49 +56,88 @@
         .row
           .col-lg-2
             figure(data-aos="fade-left")
-              img(src="@/assets/curso/t3-ic1.svg", alt="icono decorativo")
+              img(src="@/assets/curso/t1-ic1.svg", alt="icono decorativo")
           .col-lg-10
-            .h4 Servicio
-            p El conjunto de actividades establecidas para satisfacer una necesidad, es intangible, por lo tanto, se percibe de manera diferente según cada persona, siendo una postura subjetiva.
-        
+            .h4 Oferta
+            p.mb-3 La oferta turística está compuesta por los servicios que se ofrecen en los destinos o que están disponibles para la atención a turistas o pasajeros. Dentro de la oferta se puede encontrar:
+            ul.lista-ul
+              li 
+                i.fas.fa-check
+                p <b>Hoteles:</b> establecimientos de alojamiento que ofrecen al turista variedad de servicios como: restaurantes, piscinas, zonas sociales, gimnasio, guarderías, entre otros. Se pueden clasificar de diferentes maneras. En Colombia se categorizan por estrellas, pero en la mayoría de los casos y haciendo uso de las plataformas tecnológicas y redes, el turista valora los comentarios realizados por personas que vivieron la experiencia de alojarse en determinado hotel.
+              li 
+                i.fas.fa-check
+                p <b>Restaurantes:</b> el sector gastronómico es uno de los eslabones más importantes a la hora de evaluar la calidad en los destinos, por ello, en muchas ciudades se invierten recursos importantes para consolidar una oferta interesante a los sibaritas y críticos, que a su vez elevan sus conceptos en importantes publicaciones que hacen que se acreciente la llegada de nuevos comensales.
+              li 
+                i.fas.fa-check
+                p <b>Guías de turismo:</b> este prestador de servicios turísticos es tal vez el único que funciona como empresa unipersonal, ya que en su mayoría trabajan de manera independiente.
+
+              li 
+                i.fas.fa-check
+                p <b>Parques temáticos:</b> al igual que los parques de atracciones o parques naturales, los parques temáticos son igualmente importantes ya que son en su mayoría dirigidos a niños y son ellos los que demandan los servicios que son adquiridos por sus padres.
+
+              li 
+                i.fas.fa-check
+                p <b>Alquiler de autos:</b> no en todos los lugares el alquiler de autos tiene tanto éxito como en Europa o los Estados Unidos, ya que para el caso de Colombia los requisitos para acceder a un auto de alquiler son bastante complejos.
+
       .tarjeta.color-secundario.p-4(titulo="Demanda")
         .row
           .col-lg-2
             figure(data-aos="fade-left")
-              img(src="@/assets/curso/t3-ic2.svg", alt="icono decorativo")
+              img(src="@/assets/curso/t1-ic2.svg", alt="icono decorativo")
           .col-lg-10
-            .h4 
-            p
+            .h4 Demanda
+            p La demanda por su parte la componen los grupos de personas que compran o hacen uso de los servicios ofrecidos. Es así como se reconocen a los huéspedes, turistas, clientes, viajeros o pasajeros como parte de ese segmento que adquiere y consume servicios turísticos en los diferentes destinos que visitan.
 
       .tarjeta.color-secundario.p-4(titulo="Intermediarios")
         .row
           .col-lg-2
             figure(data-aos="fade-left")
-              img(src="@/assets/curso/t3-ic2.svg", alt="icono decorativo")
+              img(src="@/assets/curso/t1-ic3.svg", alt="icono decorativo")
           .col-lg-10
-            .h4 
-            p
+            .h4 Intermediarios
+            p Las agencias de viajes hacen el papel de intermediarios en el sistema turístico como parte de la cadena productiva del turismo, están reglamentadas en la ley general de turismo y son reconocidas como empresas netamente comerciales, han perdido terreno gracias a las agencias de viaje online, en inglés Online Travel Agencies (OTA) como Booking, Expedia o Despegar. A continuación, se relaciona los tres grandes grupos de agencias de viajes que existen en Colombia:
+            ul.lista-ul
+              li 
+                i.fas.fa-check
+                p Agencia de viajes mayorista
+              li 
+                i.fas.fa-check
+                p Agencia de viajes operadora
+              li 
+                i.fas.fa-check
+                p Agencia de viajes minorista o viajes y turismo            
 
       .tarjeta.color-secundario.p-4(titulo="Superestructura")
         .row
           .col-lg-2
             figure(data-aos="fade-left")
-              img(src="@/assets/curso/t3-ic2.svg", alt="icono decorativo")
+              img(src="@/assets/curso/t1-ic4.svg", alt="icono decorativo")
           .col-lg-10
-            .h4 
-            p
+            .h4 Superestructura 
+            p Aquí convergen tanto entidades territoriales que apoyan las acciones de desarrollo como políticas de apalancamiento que permiten la consolidación de recursos que finalmente se ven reflejados en resultados de transformación en el sector.
 
-      .tarjeta.color-secundario.p-4(titulo="infraestructura")
+      .tarjeta.color-secundario.p-4(titulo="Infraestructura")
         .row
           .col-lg-2
             figure(data-aos="fade-left")
-              img(src="@/assets/curso/t3-ic2.svg", alt="icono decorativo")
+              img(src="@/assets/curso/t1-ic5.svg", alt="icono decorativo")
           .col-lg-10
-            .h4 
-            p
+            .h4 Infraestructura
+            p Por su parte está relacionada con el componente físico, es decir la adecuación de la infraestructura, corresponde a la construcción de aeropuertos, senderos, hoteles, vías de acceso entre muchos otros, requeridos para prestar servicios de calidad en los diferentes destinos.
 
+    Separador
+    br
+    br
 
-
+    #t_1_2.titulo-segundo
+      h2 1.2  Geografía turística      
+    .row
+      .col-lg-7
+        p.mb-3 Es reconocida como una ciencia compleja vista desde la óptica humana y sociocultural en un territorio; sin embargo, y para el caso específico del mundo de los viajes, no se profundizará en su conceptualización, por el contrario, un profesional de aerolínea o de aeropuerto debe ser práctico a la hora de la prestación del servicio, entendiendo que no basta con conocer el entorno geográfico, ya que también hay que ubicar al viajero brindando indicaciones precisas. Como principal actividad del programa de formación, el aprendiz debe reconocer los componentes de referencia de la geografía de Colombia.
+        p
+      .col-lg-5
+        figure(data-aos="fade-up")
+          img(src="@/assets/curso/t1-i3.jpg", alt="Manos señalando un punto en el mapa mundial")
 
 
 
